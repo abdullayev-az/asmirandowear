f204d4319e31696f4627ff6c5da9c6896bc24cb4
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2026-04-25 20:58:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
+// Səhifə tam yüklənəndə loaderi gizlət
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader-menu");
+
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.visibility = "hidden";
+  }, 1000); // 0.5 saniyə gecikmə (istəsən dəyiş)
+});
+
+
+// Refresh və ya başqa səhifəyə keçəndə loaderi yenə göstər
+window.addEventListener("beforeunload", () => {
+  const loader = document.querySelector(".loader-menu");
+
+  loader.style.opacity = "1";
+  loader.style.visibility = "visible";
+});
