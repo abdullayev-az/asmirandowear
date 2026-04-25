d4385d41a5b49658413ea4d9c4c1d240933a3fd3
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2026-04-25 21:03:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -7,21 +7,36 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
-// Səhifə tam yüklənəndə loaderi gizlət
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+// Loader başlayanda scroll-u bağla
+document.body.classList.add("no-scroll");
+
+// Səhifə yüklənəndə loaderi gizlət + scroll aç
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader-menu");
 
   setTimeout(() => {
     loader.style.opacity = "0";
     loader.style.visibility = "hidden";
-  }, 1000); // 0.5 saniyə gecikmə (istəsən dəyiş)
+
+    document.body.classList.remove("no-scroll"); // scroll açılır
+  }, 1500);
 });
 
-
-// Refresh və ya başqa səhifəyə keçəndə loaderi yenə göstər
+// Refresh zamanı yenə bağla
 window.addEventListener("beforeunload", () => {
-  const loader = document.querySelector(".loader-menu");
+  document.body.classList.add("no-scroll");
 
+  const loader = document.querySelector(".loader-menu");
   loader.style.opacity = "1";
   loader.style.visibility = "visible";
 });
